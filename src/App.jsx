d12cc6cb4cb1cc66cdd391bd3be957e439e83755
Tarun Vaka <102230411+Tarunvaka@users.noreply.github.com>
2024-06-14 20:58:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navi from './components/navigation'
+import Hero from './components/Hero'
+
+const App = () =>{
+  return (<div>
+    <Navi />
+    <Hero />
+    </div>
+  )
+}
+
+export default App
